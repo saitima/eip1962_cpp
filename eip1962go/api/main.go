faed46c643586f1cf36e5db850f9c38cdea8372f
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-01-20 13:16:05 +0300

    make few test vectors. Move alloc to constants

--- eip1962go/api/main.go
+++ eip1962go/api/main.go
@@ -9,11 +9,16 @@ import (
 	"unsafe"
 )
 
+const (
+	MAX_OUTPUT_LEN = 256*3*2
+	MAX_ERR_LEN = 256
+)
+
 //export c_perform_operation
 func c_perform_operation(op C.char, i *C.char, i_len uint32, o *C.char, o_len *uint32, e *C.char, e_len *uint32) C.int {
 	buf := C.GoBytes(unsafe.Pointer(i), C.int(i_len))
-	oBuff := C.GoBytes(unsafe.Pointer(o), C.int(768))
-	eBuff := C.GoBytes(unsafe.Pointer(e), C.int(256))
+	oBuff := C.GoBytes(unsafe.Pointer(o), C.int(MAX_OUTPUT_LEN))
+	eBuff := C.GoBytes(unsafe.Pointer(e), C.int(MAX_ERR_LEN))
 
 	opType := int(op)
 	res, err := new(eip.API).Run(opType, buf)
