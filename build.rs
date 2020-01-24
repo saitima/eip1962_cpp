3b3bd735dbd3559adca98d75cd7d311a882bb615
Alex Vlasov <alex.m.vlasov@gmail.com>
2020-01-24 18:50:23 +0300

    should rebuild every time

--- build.rs
+++ build.rs
@@ -3,10 +3,15 @@ use std::env;
 // use std::path::Path;
 
 fn main() {
-    let out_dir = env::var("OUT_DIR").unwrap();
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=make.sh");
 
+    let out_dir = env::var("OUT_DIR").unwrap();
     Command::new("sh").args(&["./make.sh"]).status().unwrap();
 
     println!("cargo:rustc-link-search=native={}", out_dir);
     println!("cargo:rustc-link-lib=static=eip1962");
+
+    Command::new("touch").args(&["build.rs"]).status().unwrap();
+    Command::new("touch").args(&["make.sh"]).status().unwrap();
 }
